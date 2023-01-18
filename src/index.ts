f0f9a6fb19f880e7bfa21a34b0a2095ff85521a9
Michael DeGori <michael.degori@gmail.com>
2023-01-18 13:31:56 -0500

    changed running port

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.get('/', (req: express.Request, res: express.Response) => {
 	res.send('Hello World!');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
