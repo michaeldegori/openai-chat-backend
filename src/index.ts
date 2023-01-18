bb13e2d403b37561d1b167e25188852a7e66b3db
Michael DeGori <michael.degori@gmail.com>
2023-01-18 13:11:09 -0500

    set up node project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
