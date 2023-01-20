25061df83dcd4c952cdee055c8a69d6de36fe841
Michael DeGori <michael.degori@gmail.com>
2023-01-20 11:58:14 -0500

    setup

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import chatGPTRouter from './src/routes/chatgpt';
 
 const app = express();
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get('/', (_req: express.Request, res: express.Response) => {
 	res.send('Hello World!');
 });
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/chatgpt', chatGPTRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => {
---
+++ src/routes/chatgpt/index.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const chatGPTRouter = Router();
+
+chatGPTRouter.post('/query', async function (req, res) {
+	if (!configuration.apiKey) {
+		res.status(500).json({
+			error: {
+				message:
+					'OpenAI API key not configured, please follow instructions in README.md',
+			},
+		});
+		return;
+	}
+	const { query } = req.body;
+
+	try {
+		const completion = await openai.createCompletion({
+			model: 'text-davinci-003',
+			prompt: query,
+			temperature: 1,
+			max_tokens: 100,
+		});
+		res.status(200).json({ result: completion.data.choices[0].text });
+	} catch (error: any) {
+		if (error.response) {
+			console.error(error.response.status, error.response.data);
+			res.status(error.response.status).json(error.response.data);
+		} else {
+			console.error(`Error with OpenAI API request: ${error.message}`);
+			res.status(500).json({
+				error: {
+					message: 'An error occurred during your request.',
+				},
+			});
+		}
+	}
+});
+
+export default chatGPTRouter;
