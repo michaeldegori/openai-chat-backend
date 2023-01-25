605fc2cfd3cb32bf27dc767c89e9bb714bbb32e7
Michael DeGori <michael.degori@gmail.com>
2023-01-25 09:45:53 -0500

    test

--- config/openai_api_config.ts
+++ config/openai_api_config.ts
@@ -5,6 +5,7 @@ let openai: OpenAIApi | undefined;
 
 const initializeOpenai = async () => {
     const apiKey = await fetchParameter('OPENAI_API_KEY');
+    console.log({ apiKey });
     const configuration = new Configuration({
         apiKey,
     });
